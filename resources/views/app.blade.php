2be5144f0147d67a76bbde9abe77bdfd48b2b500
Allan Justine Mascariñas <labya200@gmail.com>
2024-12-27 06:48:39 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="font-size: 16px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <title>My App Project</title>
+    @vite
+</head>
+
+<body class="antialiased bg-gray-100">
+    @inertia
+</body>
+
+</html>
